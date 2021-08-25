089788a89ef70178191c21be8682202c924cbaa5
Philip Efem <pefemjnr@gmail.com>
2021-08-25 11:08:18 +0100

    added implementation to product_details.py

--- product_details.py
+++ product_details.py
@@ -8,23 +8,57 @@ from pathlib import Path
 
 class ProductDetails:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, start) -> None:
+        self.products = product_links.Products(start)
+        self.products.get_product_links()
     
 
     def get_links_from_db(self):
-        pass
+        product_links = []
+
+        with sqlite3.connect("db.exify") as conn:
+            command = "SELECT * FROM tb_productlinks"
+            cursor = conn.execute(command)
+            for row in cursor:
+                product_links.append(row[0])
+        
+        return product_links
 
 
-    def process_links(self):
-        pass
+    def process_links(self, links):
+        products = []
+
+        for link in links:
+            response = requests.get(link, self.products.headers)
+
+            page_soup = BeautifulSoup(response.text, "html.parser")
+            outter_wrap = page_soup.select_one(".caption")
+
+            product_name = outter_wrap.select("h4")[1].get_text()
+            product_description = outter_wrap.select_one(".description").get_text()
+            product_price = outter_wrap.select("h4")[0].get_text()
+
+            item = {
+                "name": product_name,
+                "description": product_description,
+                "price": product_price
+            }
+
+            products.append(item)
+        
+        return products
 
 
-    def write_to_file(self):
-        pass
+    def write_to_file(self, products):
+
+        data = json.dumps(products)
+        Path("products.json").write_text(data)
 
 
 
 
 if __name__ == "__manin__":
-    pass
+    details = ProductDetails("https://webscraper.io/test-sites/e-commerce/allinone")
+    a = details.get_links_from_db()
+    b = details.process_links(a)
+    details.write_to_file(b)
